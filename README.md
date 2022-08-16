4b6363fdad0cdbc1657a59ab9403e0d33332bb64
francovelazquez2011 <francovelazquez2011@gmail.com>
2022-08-16 20:00:39 -0300

    T.P

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # TP-1
+ 
+# Alumno: Franco Ignacio Velazquez 
+
+Este trabajo fue realizado con Javascript, Html y Css, utilizando el framework de Bootstrap 5.
+Consiste en una aplicación de E-commerce donde el administrador puede agregar o eliminar productos de su carrito de compras.
+ 
+Instrucciones:
+ 
+A continuación un enlace a mi github donde se encuentra el repositorio de este proyecto (rama main):
+ 
+https://github.com/Francov11/Sprint-Project-1
+ 
+Instrucciones:
+ 
+Descargar el repositorio de github o utilizar el comando 'git clone' en su terminal.
+ 
+Opción 1: Cliquear el Archivo llamado 'index.html', este lo redireccionará a su navegador.
+ 
+Opción 2: Instalar en vscode la extensión llamada 'Live server', haciendo click derecho sobre el archivo 'index.html' le aparece una
+          opción 'Open with live server', está lo redirecciona a su navegador.
+
---
+++ src/app.js
@@ -0,0 +1,102 @@
+//Declaracion de variables
+const nameInput = document.getElementById('nameInput')
+const priceInput = document.getElementById('priceInput')
+const sumbitButton = document.getElementById('sumbitButton')
+const nameList = document.querySelector('.nameList')
+const priceList = document.querySelector('.priceList')
+const amountList = document.querySelector('.amountList')
+const subtotalList = document.querySelector('.subtotalList')
+const deleteLastButton = document.getElementById('deleteLastButton')
+const deleteAllButton = document.getElementById('deleteAllButton')
+const totalButton = document.getElementById('totalButton')
+const amountInput = document.getElementById('amountInput')
+
+//Arrays
+let arrNameProduct = []
+let arrAmountProduct = []
+let arrPriceProduct = []
+let arrSubtotalProduct = []
+
+let contador = 0
+
+//Funcion Actualizar lista
+const updateList = () => {
+    console.log(arrNameProduct)
+    console.log(arrPriceProduct)
+    console.log(arrSubtotalProduct)
+    nameList.innerHTML = ""
+    amountList.innerHTML = ""
+    priceList.innerHTML = ""
+    subtotalList.innerHTML = ""
+    for(i = 0; i < arrNameProduct.length; i++){
+        let nameProducts = document.createElement('li')
+        nameProducts.innerHTML = arrNameProduct[i]
+        nameList.append(nameProducts)
+    }
+    for(i = 0; i < arrAmountProduct.length; i++){
+        let amountProducts = document.createElement('li')
+        amountProducts.innerHTML = arrAmountProduct[i]
+        amountList.append(amountProducts)
+    }
+    for(i = 0; i < arrPriceProduct.length; i++){
+        let priceProducts = document.createElement('li')
+        priceProducts.innerHTML = arrPriceProduct[i]
+        priceList.append(priceProducts)
+    }
+    for(i = 0; i < arrPriceProduct.length; i++){
+        arrSubtotalProduct = arrAmountProduct.map(product => product * arrPriceProduct[i])
+        let subTotalProduct = document.createElement('li')
+        subTotalProduct.innerHTML = arrSubtotalProduct[i]
+        subtotalList.append(subTotalProduct)
+    }
+}
+
+//Funcion Eliminar Ultimo Producto
+const deleteLastProduct = () => {
+    arrNameProduct.pop();
+    arrPriceProduct.pop();
+    arrAmountProduct.pop();
+    arrSubtotalProduct.pop();
+    nameList.removeChild(nameList.lastChild)
+    priceList.removeChild(priceList.lastChild)
+    amountList.removeChild(amountList.lastChild)
+    subtotalList.removeChild(subtotalList.lastChild)
+}
+
+//Funcion Añadir Producto
+const addProduct = () => {
+    arrNameProduct.push(nameInput.value);
+    arrAmountProduct.push(Number(amountInput.value));
+    arrPriceProduct.push(Number(priceInput.value));
+    updateList();
+
+}
+
+//Funcion Borrar todos los productos
+const deleteAllProducts = () => {
+    arrNameProduct = [];
+    arrPriceProduct = [];
+    arrAmountProduct = [];
+    arrSubtotalProduct = [];
+    nameList.innerHTML = "";
+    priceList.innerHTML = "";
+    amountList.innerHTML = "";
+    subtotalList.innerHTML = "";
+}
+
+//Funcion calcular total de la compra
+const totalProducts = () => {
+    console.log(arrSubtotalProduct)
+    let total = 0
+        for(i = 0; i < arrSubtotalProduct.length; i++){
+
+            total = total + arrSubtotalProduct[i]
+        }
+    alert('Total de su compra: '+ total)
+}
+
+//Eventos
+sumbitButton.addEventListener('click', addProduct)
+deleteLastButton.addEventListener('click', deleteLastProduct)
+deleteAllButton.addEventListener('click', deleteAllProducts)
+totalButton.addEventListener('click', totalProducts)
